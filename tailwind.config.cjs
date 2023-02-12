e8edaacf22b9c29555edeeb454b183f2c52fbd4b
Youssef Mrabet <youssef.mrabet@enetcom.u-sfax.tn>
2023-02-12 13:07:44 +0100

    Redesign

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'jb' : ['JetBrains Mono', 'Helvetica'],
+		},
+		backgroundImage: {
+			'about-pattern': "url('/about.svg')"
+		},
 		extend: {},
 	},
 	plugins: [],
